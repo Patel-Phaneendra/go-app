00f4d8df08b42872340dac6f00e81b883272c27b
Patel-Phaneendra <99329272+Patel-Phaneendra@users.noreply.github.com>
2022-06-27 17:55:24 +0530

    Update login-main.go
    code changed

--- login-main.go
+++ login-main.go
@@ -1,81 +1,75 @@
 package main
 
 import (
-	"github.com/pquerna/otp"
-	"github.com/pquerna/otp/totp"
-
-	"bufio"
-	"bytes"
-	"encoding/base32"
 	"fmt"
-	"image/png"
-	"io/ioutil"
-	"os"
-	"time"
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"log"
+	"strings"
 )
 
-func display(key *otp.Key, data []byte) {
-	fmt.Printf("Issuer:       %s\n", key.Issuer())
-	fmt.Printf("Account Name: %s\n", key.AccountName())
-	fmt.Printf("Secret:       %s\n", key.Secret())
-	fmt.Println("Writing PNG to qr-code.png....")
-	ioutil.WriteFile("qr-code.png", data, 0644)
-	fmt.Println("")
-	fmt.Println("Please add your TOTP to your OTP Application now!")
-	fmt.Println("")
-}
-
-func promptForPasscode() string {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter Passcode: ")
-	text, _ := reader.ReadString('\n')
-	return text
-}
-
-// Demo function, not used in main
-// Generates Passcode using a UTF-8 (not base32) secret and custom parameters
-func GeneratePassCode(utf8string string) string {
-	secret := base32.StdEncoding.EncodeToString([]byte(utf8string))
-	passcode, err := totp.GenerateCodeCustom(secret, time.Now(), totp.ValidateOpts{
-		Period:    30,
-		Skew:      1,
-		Digits:    otp.DigitsSix,
-		Algorithm: otp.AlgorithmSHA512,
-	})
-	if err != nil {
-		panic(err)
-	}
-	return passcode
-}
-
+// !+input
+const hello = `
+package main
+import "fmt"
+// append
 func main() {
-	key, err := totp.Generate(totp.GenerateOpts{
-		Issuer:      "Example.com",
-		AccountName: "alice@example.com",
-	})
-	if err != nil {
-		panic(err)
-	}
-	// Convert TOTP key into a PNG
-	var buf bytes.Buffer
-	img, err := key.Image(200, 200)
-	if err != nil {
-		panic(err)
-	}
-	png.Encode(&buf, img)
+        // fmt
+        fmt.Println("Hello, world")
+        // main
+        main, x := 1, 2
+        // main
+        print(main, x)
+        // x
+}
+// x
+`
 
-	// display the QR code to the user.
-	display(key, buf.Bytes())
+//!-input
 
-	// Now Validate that the user's successfully added the passcode.
-	fmt.Println("Validating TOTP...")
-	passcode := promptForPasscode()
-	valid := totp.Validate(passcode, key.Secret())
-	if valid {
-		println("Valid passcode!")
-		os.Exit(0)
-	} else {
-		println("Invalid passcode!")
-		os.Exit(1)
+// !+main
+func main() {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "hello.go", hello, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err) // parse error
+	}
+
+	conf := types.Config{Importer: importer.Default()}
+	pkg, err := conf.Check("cmd/hello", fset, []*ast.File{f}, nil)
+	if err != nil {
+		log.Fatal(err) // type error
+	}
+
+	// Each comment contains a name.
+	// Look up that name in the innermost scope enclosing the comment.
+	for _, comment := range f.Comments {
+		pos := comment.Pos()
+		name := strings.TrimSpace(comment.Text())
+		fmt.Printf("At %s,\t%q = ", fset.Position(pos), name)
+		inner := pkg.Scope().Innermost(pos)
+		if _, obj := inner.LookupParent(name, pos); obj != nil {
+			fmt.Println(obj)
+		} else {
+			fmt.Println("not found")
+		}
 	}
 }
+
+//!-main
+
+/*
+//!+output
+$ go build golang.org/x/example/gotypes/lookup
+$ ./lookup
+At hello.go:6:1,        "append" = builtin append
+At hello.go:8:9,        "fmt" = package fmt
+At hello.go:10:9,       "main" = func cmd/hello.main()
+At hello.go:12:9,       "main" = var main int
+At hello.go:14:9,       "x" = var x int
+At hello.go:16:1,       "x" = not found
+//!-output
+*/
