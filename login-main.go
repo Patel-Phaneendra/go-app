1b7200f830ac7cb80dc2b245fd7be258e6e0a663
Patel-Phaneendra <99329272+Patel-Phaneendra@users.noreply.github.com>
2022-06-27 16:49:06 +0530

    Update login-main.go
    code changed

--- login-main.go
+++ login-main.go
@@ -1,58 +1,22 @@
+// Go program to illustrate the
+// use of function
 package main
+import "fmt"
 
-import (
-	"errors"
-	"fmt"
-	"log"
+// area() is used to find the
+// area of the rectangle
+// area() function two parameters,
+// i.e, length and width
+func area(length, width int)int{
+	
+	Ar := length* width
+	return Ar
+}
 
-	"net/http"
-	"os"
-
-	"github.com/aristat/http-server/internal/app/router"
-	"github.com/joho/godotenv"
-	"github.com/urfave/cli/v2"
-)
-
+// Main function
 func main() {
-	app := &cli.App{
-		Commands: []*cli.Command{
-			{
-				Name:        "start",
-				Aliases:     []string{"s"},
-				Usage:       "run server",
-				Description: "http server",
-				Flags: []cli.Flag{
-					&cli.StringFlag{Name: "envFile", Value: ".env.development"},
-				},
-				Action: func(c *cli.Context) error {
-					fileName := c.String("envFile")
 
-					err := godotenv.Load(fileName)
-					if err != nil {
-						return errors.New("Error loading .env file")
-					}
-
-					r, cleanup, err := router.Build()
-					defer cleanup()
-					if err != nil {
-						return err
-					}
-
-					s := &http.Server{
-						Handler: r,
-						Addr:    fmt.Sprintf(":%s", os.Getenv("PORT")),
-					}
-
-					err = s.ListenAndServe()
-					return err
-				},
-			},
-		},
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal("cli error ", err)
-		os.Exit(1)
-	}
+// Display the area of the rectangle
+// with method calling
+fmt.Printf("Area of rectangle is : %d", area(12, 10))
 }
