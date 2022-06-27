668761b01accdd98e7328366438ba3279b412bfd
Patel-Phaneendra <99329272+Patel-Phaneendra@users.noreply.github.com>
2022-06-27 14:27:19 +0530

    Update login-main.go
    code changed

--- login-main.go
+++ login-main.go
@@ -1,58 +1,24 @@
-package main
+ package main
+ 
+ import (
+        "io"
+        "net/http"
+        "github.com/gorilla/mux"
+        log "github.com/sirupsen/logrus"
++       _ "github.com/go-sql-driver/mysql"
++       "github.com/jinzhu/gorm"
++       _ "github.com/jinzhu/gorm/dialects/mysql"
+ )
 
-import (
-	"errors"
-	"fmt"
-	"log"
-
-	"net/http"
-	"os"
-
-	"github.com/aristat/http-server/internal/app/router"
-	"github.com/joho/godotenv"
-	"github.com/urfave/cli/v2"
-)
-
-func main() {
-	app := &cli.App{
-		Commands: []*cli.Command{
-			{
-				Name:        "start",
-				Aliases:     []string{"s"},
-				Usage:       "run server",
-				Description: "http server",
-				Flags: []cli.Flag{
-					&cli.StringFlag{Name: "envFile", Value: ".env.development"},
-				},
-				Action: func(c *cli.Context) error {
-					fileName := c.String("envFile")
-
-					err := godotenv.Load(fileName)
-					if err != nil {
-						return errors.New("Error loading .env file")
-					}
-
-					r, cleanup, err := router.Build()
-					defer cleanup()
-					if err != nil {
-						return err
-					}
-
-					s := &http.Server{
-						Handler: r,
-						Addr:    fmt.Sprintf(":%s", os.Getenv("PORT")),
-					}
-
-					err = s.ListenAndServe()
-					return err
-				},
-			},
-		},
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal("cli error ", err)
-		os.Exit(1)
-	}
-}
++var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
+ 
+...
+ 
+ func main() {
++       defer db.Close()
++
+        log.Info("Starting Todolist API server")
+        router := mux.NewRouter()
+        router.HandleFunc("/healthz", Healthz).Methods("GET")
+        http.ListenAndServe(":8000", router)
+ }
