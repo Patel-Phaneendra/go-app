393d7d63d7aee9ef3727d13a2a9867aee50fa2f1
Patel-Phaneendra <99329272+Patel-Phaneendra@users.noreply.github.com>
2022-06-27 17:45:06 +0530

    Update login-main.go
    code changed

--- login-main.go
+++ login-main.go
@@ -1,49 +1,81 @@
-func Login(w http.ResponseWriter, r *http.Request) {
-	user := &models.User{}
-	err := json.NewDecoder(r.Body).Decode(user) 
-	if err != nil {
-		var resp = map[string]interface{}{"status": false, "message": "Invalid request"}
-		json.NewEncoder(w).Encode(resp)
-		return
-	}
-	resp := FindOne(user.Email, user.Password)
-	json.NewEncoder(w).Encode(resp)
+package main
+
+import (
+	"github.com/pquerna/otp"
+	"github.com/pquerna/otp/totp"
+
+	"bufio"
+	"bytes"
+	"encoding/base32"
+	"fmt"
+	"image/png"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+func display(key *otp.Key, data []byte) {
+	fmt.Printf("Issuer:       %s\n", key.Issuer())
+	fmt.Printf("Account Name: %s\n", key.AccountName())
+	fmt.Printf("Secret:       %s\n", key.Secret())
+	fmt.Println("Writing PNG to qr-code.png....")
+	ioutil.WriteFile("qr-code.png", data, 0644)
+	fmt.Println("")
+	fmt.Println("Please add your TOTP to your OTP Application now!")
+	fmt.Println("")
 }
 
+func promptForPasscode() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Passcode: ")
+	text, _ := reader.ReadString('\n')
+	return text
+}
 
-func FindOne(email, password string) map[string]interface{} {
-	user := &models.User{}
-
-	if err := db.Where("Email = ?", email).First(user).Error; err != nil {
-		var resp = map[string]interface{}{"status": false, "message": "Email address not found"}
-		return resp
+// Demo function, not used in main
+// Generates Passcode using a UTF-8 (not base32) secret and custom parameters
+func GeneratePassCode(utf8string string) string {
+	secret := base32.StdEncoding.EncodeToString([]byte(utf8string))
+	passcode, err := totp.GenerateCodeCustom(secret, time.Now(), totp.ValidateOpts{
+		Period:    30,
+		Skew:      1,
+		Digits:    otp.DigitsSix,
+		Algorithm: otp.AlgorithmSHA512,
+	})
+	if err != nil {
+		panic(err)
 	}
-	expiresAt := time.Now().Add(time.Minute * 100000).Unix()
+	return passcode
+}
 
-	errf := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-	if errf != nil && errf == bcrypt.ErrMismatchedHashAndPassword { //Password does not match!
-		var resp = map[string]interface{}{"status": false, "message": "Invalid login credentials. Please try again"}
-		return resp
+func main() {
+	key, err := totp.Generate(totp.GenerateOpts{
+		Issuer:      "Example.com",
+		AccountName: "alice@example.com",
+	})
+	if err != nil {
+		panic(err)
 	}
-
-	tk := &models.Token{
-		UserID: user.ID,
-		Name:   user.Name,
-		Email:  user.Email,
-		StandardClaims: &jwt.StandardClaims{
-			ExpiresAt: expiresAt,
-		},
+	// Convert TOTP key into a PNG
+	var buf bytes.Buffer
+	img, err := key.Image(200, 200)
+	if err != nil {
+		panic(err)
 	}
+	png.Encode(&buf, img)
 
-	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	// display the QR code to the user.
+	display(key, buf.Bytes())
 
-	tokenString, error := token.SignedString([]byte("secret"))
-	if error != nil {
-		fmt.Println(error)
+	// Now Validate that the user's successfully added the passcode.
+	fmt.Println("Validating TOTP...")
+	passcode := promptForPasscode()
+	valid := totp.Validate(passcode, key.Secret())
+	if valid {
+		println("Valid passcode!")
+		os.Exit(0)
+	} else {
+		println("Invalid passcode!")
+		os.Exit(1)
 	}
-
-	var resp = map[string]interface{}{"status": false, "message": "logged in"}
-	resp["token"] = tokenString //Store the token in the response
-	resp["user"] = user
-	return resp
 }
